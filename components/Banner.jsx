838eee611826bbb9d70f954203477a1e72ddb9ee
abdul-razak alhassan <aabdulrazak22@gmail.com>
2026-01-14 07:20:51 +0000

    add comments

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -12,26 +12,26 @@ const Banner = () => {
       />
       <div className="flex flex-col items-center justify-center text-center space-y-2 px-4 md:px-0">
         <h2 className="text-2xl md:text-3xl font-semibold max-w-[290px]">
-          Level Up Your Gaming Experience
+          Level Up Your Agro products
         </h2>
         <p className="max-w-[343px] font-medium text-gray-800/60">
-          From immersive sound to precise controls—everything you need to win
+          Aqeela Services
         </p>
         <button className="group flex items-center justify-center gap-1 px-12 py-2.5 bg-orange-600 rounded text-white">
           Buy now
           <Image className="group-hover:translate-x-1 transition" src={assets.kk5} alt="arrow_icon_white" />
         </button>
       </div>
-      {/* <Image
+      <Image
         className="hidden md:block max-w-80"
-        src={assets.md_controller_image}
+        src={assets.k6}
         alt="md_controller_image"
-      /> */}
-      {/* <Image
+      />
+      <Image
         className="md:hidden"
-        src={assets.sm_controller_image}
+        src={assets.k12}
         alt="sm_controller_image"
-      /> */}
+      />
     </div>
   );
 };
